5507549b9a535c26f94250dffcf3fcc565391b26
Andrey <razorvin.andrey2000@gmail.com>
2022-09-23 23:32:20 +0300

    sotable table and column chart

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,65 @@
 export default class NotificationMessage {
+  static activeNotification;
+  element;
+  timer;
 
+  constructor(message = '', {duration = 2000, type = 'success'} = {}) {
+
+    this.duration = duration;
+    this.message = message;
+    this.type = type;
+    this.durationInSecond = (duration / 1000) + 's';
+    this.render();
+  }
+
+  get template() {
+
+    return `
+        <div class="notification ${this.type}" style="--value: ${this.durationInSecond}">
+            <div class="timer"></div>
+            <div class="inner-wrapper">
+                <div class="notification-header">
+                    notification
+                </div>
+                <div class="notification-body">${this.message}</div>
+            </div>
+        </div>
+    `;
+  }
+
+  render() {
+    const element = document.createElement('div');
+
+    element.innerHTML = this.template;
+
+    this.element = element.firstElementChild;
+  }
+
+  show(parent = document.body) {
+    if (NotificationMessage.activeNotification) {
+      NotificationMessage.activeNotification.remove();
+    }
+
+    parent.append(this.element);
+
+    this.timer = setTimeout(() => {
+      this.remove();
+    }, this.duration);
+
+    NotificationMessage.activeNotification = this;
+  }
+
+  remove() {
+    clearTimeout(this.timer);
+
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+    NotificationMessage.activeNotification = null;
+  }
 }
